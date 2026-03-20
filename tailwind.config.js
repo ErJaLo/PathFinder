6c9a3061e4e4b3667e6e4ed43440b7f291727cfe
RogerMuntane <rogermuntane123@gmail.com>
2026-03-20 19:21:29 +0100

    tailwind en scss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/css/**/*.scss',
+        './resources/css/**/*.css',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
