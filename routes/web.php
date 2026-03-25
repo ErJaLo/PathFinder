e16969486d0c51054056803354a6aac1acd633e6
NicoMisz <nicommisz@gmail.com>
2026-03-25 18:57:31 +0100

    fix: laravel route order

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/explorar', [ExperienciaController::class, 'index'])->name('explorar.index');
-Route::get('/experiencies/{post}', [ExperienciaController::class, 'show'])->name('experiencies.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::get('/experiencies/crear', [ExperienciaController::class, 'create'])->name('experiencies.create');
     Route::post('/experiencies', [ExperienciaController::class, 'store'])->name('experiencies.store');
 });
+
+Route::get('/experiencies/{post}', [ExperienciaController::class, 'show'])->name('experiencies.show');
 
 Route::middleware(['auth', 'role:moderator,admin'])->prefix('admin')->group(function () {
     Route::inertia('/', 'admin/index')->name('admin.index');
