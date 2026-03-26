1eb46c2381fa9c181ea0251d705cce5c21303a7a
ErJaLo <156296701+ErJaLo@users.noreply.github.com>
2026-03-26 16:19:38 +0100

    Usercontroller+routing for usercontroller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = DB::table('users')
+        ->selectRaw("
+            users.id,
+            users.name,
+            users.email,
+            users.created_at,
+            COUNT(posts.user_id) as total_posts,
+            CASE
+                WHEN users.active = 1 THEN 'active'
+                WHEN users.active = 0 AND users.suspended_at IS NOT NULL THEN 'deleted'
+                WHEN users.active = 0 THEN 'inactive'
+            END as status
+        ")
+        ->leftJoin('posts', 'posts.user_id', '=', 'users.id')
+        ->groupBy('users.id', 'users.name', 'users.email', 'users.active', 'users.suspended_at')
+        ->get();
+        return Inertia::render('admin/users', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -11,6 +12,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 Route::middleware(['auth', 'role:moderator,admin'])->prefix('admin')->group(function () {
     Route::inertia('/', 'admin/index')->name('admin.index');
+    Route::get('users', [UserController::class, 'index'])->name('admin.users');
 });
-
+    
 require __DIR__ . '/settings.php';
