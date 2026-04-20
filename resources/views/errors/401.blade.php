77a6d6d3cb494a58590c7d2dd44f6442203ba86c
ErJaLo <156296701+ErJaLo@users.noreply.github.com>
2026-04-20 17:16:43 +0200

    translate errors into catalan

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Unauthorized'))
+@section('title', __('No autoritzat'))
 @section('code', '401')
-@section('message', __('Unauthorized'))
+@section('message', __('No autoritzat'))
--- resources/views/errors/402.blade.php
+++ resources/views/errors/402.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Payment Required'))
+@section('title', __('Es requereix pagament'))
 @section('code', '402')
-@section('message', __('Payment Required'))
+@section('message', __('Es requereix pagament'))
